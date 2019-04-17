8c9c4e73add82e15255257b92bc5d0c1f7a76917
vonThule <alexpopp82@gmail.com>
2019-04-17 20:47:00 +0200

    Added solution to problem 33 in chapter 3.

---
+++ src/chapter_3/problem_33.c
@@ -0,0 +1,39 @@
+/*
+ * Filename: problem_33.c
+ * Project: C Language Workbook Problems
+ * Section: Chapter 3
+ * Author: Alexander Popp
+ * Date created: 04/17/2019
+ * Date last modified: 04/17/2019
+ * 
+ * Description:
+ *     Problem #33: Write a program that determines the number of empty spaces,
+ *     dots, and interpunction signs (,;:!?) in the given text.
+ */
+
+
+#include <stdio.h>
+
+int main() {
+    char chars;
+    int spaces = 0, dots = 0, interp = 0;
+    printf("Enter text >> ");
+    while((chars = getchar()) != EOF) {
+        /* EOF - CTRL+D */
+        switch(chars) {
+            case ' ': case '\t':
+                spaces++;
+                break;
+            case '.':
+                dots++;
+                break;
+            case ',': case ';': case ':': case '!': case '?':
+                interp++;
+                break;
+        }
+    }
+    printf("Number of empty apces: %d,\n", spaces);
+    printf("Number of dots: %d,\n", dots);
+    printf("Number of interpunction signs: %d.\n", interp);
+    return 0;
+}
